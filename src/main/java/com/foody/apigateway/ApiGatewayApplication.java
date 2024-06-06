1850caa6d2ddd2e938af0f15a3a2ef5024566f62
Ivana Nedelkova <i.nedelkova@student.fontys.nl>
2024-06-06 16:33:15 +0200

    enable async

--- src/main/java/com/foody/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/foody/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.foody.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
